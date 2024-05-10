5966e0abbd2d33d4c8564863cbb0ed4bf807367a
taylosstls <luisgustavogto@gmail.com>
2024-05-10 15:22:59 -0300

    Create study and first lesson IMC

---
+++ imc-calc-study/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Menu from "@/components/menu";
+
+export default function HomePage() {
+  return (
+    <main>
+      <Menu />
+      <h1 className="text-2xl font-bold">Home</h1>
+    </main>
+  );
+}
