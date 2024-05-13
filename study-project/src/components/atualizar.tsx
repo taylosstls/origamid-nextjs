d0d0a01e68d932048b92b16375da9f7f796d1c3d
taylosstls <luisgustavogto@gmail.com>
2024-05-13 17:27:35 -0300

    Create revalidate path for URL and Tags

--- study-project/src/components/atualizar.tsx
+++ study-project/src/components/atualizar.tsx
@@ -1,8 +1,23 @@
 'use client';
-import { revalidatePathAction } from '@/actions/revalidate-path';
+import { revalidatePathAction, revalidateTagAction } from '@/actions/revalidate-path';
+import { useEffect } from 'react';
 
 export default function Atualizar() {
+  function handleClick() {
+    revalidatePathAction('/acoes-cache')
+    revalidateTagAction('acoes')
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      revalidatePathAction('/acoes-cache')
+    }, 20 * 1000) // Atualiza sozinho a cada 20 segudos
+
+    return () => clearInterval(intervalId)
+  })
+
+
   return (
-    <button onClick={() => revalidatePathAction('/acoes-cache')}>Atualizar</button>
+    <button onClick={handleClick}>Atualizar</button>
   );
 }
