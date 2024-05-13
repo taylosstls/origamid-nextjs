a91a90d4839d7e14ce8a89c487db5e83a53010f1
taylosstls <luisgustavogto@gmail.com>
2024-05-13 11:46:35 -0300

    Create api.http to send request test

--- study-project/src/app/api/route.ts
+++ study-project/src/app/api/route.ts
@@ -1,3 +1,5 @@
+import { NextRequest } from "next/server";
+
 export async function GET() {
   const response = await fetch('https://api.origamid.online/vendas', {
     headers: {
@@ -7,4 +9,11 @@ export async function GET() {
   const vendas = await response.json();
 
   return Response.json(vendas);
+}
+
+export async function POST(request: NextRequest) {
+  const param = request.nextUrl.searchParams.get('busca');
+  const body = await request.json();
+
+  return Response.json({ body });
 }
