545e020609b5683b490471f9ed0bb4efce2333fc
taylosstls <luisgustavogto@gmail.com>
2024-05-14 13:23:46 -0300

    Add useForm formState

--- study-project/src/actions/adicionar-produto.tsx
+++ study-project/src/actions/adicionar-produto.tsx
@@ -1,10 +1,25 @@
-// actions/adicionar-produto.tsx
+
+// actions/adicionar-produto.ts
 'use server';
+
 import { ProductParams } from '@/app/produtos/[id]/page';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-export async function adicionarProduto(formData: FormData) {
+// unknown = não sabe como é o tipo de formato que está sendo enviado, não necessariamente pode ser uma string
+function validarNome(nome: unknown) {
+  return typeof nome === 'string' && nome.length > 1;
+}
+
+function validarPreco(preco: unknown) {
+  return typeof preco === 'number' && preco >= 1;
+}
+
+export async function adicionarProduto(
+  state: { errors: string[] },
+  formData: FormData,
+) {
+
   const produto: ProductParams = {
     nome: formData.get('nome') as string,
     preco: Number(formData.get('preco')),
@@ -13,15 +28,34 @@ export async function adicionarProduto(formData: FormData) {
     importado: formData.get('importado') ? 1 : 0,
   };
 
-  const response = await fetch('https://api.origamid.online/produtos', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(produto),
-  });
-  await response.json();
- 
+  let errors = [];
+  if (!validarNome(produto.nome)) errors.push('Nome inválido.');
+  if (!validarPreco(produto.preco)) errors.push('Preço inválido.');
+
+  if (errors.length > 0) return { errors };
+
+  try {
+    const response = await fetch('https://api.origamid.online/produtos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(produto),
+    });
+    
+    if (!response.ok) throw new Error('Erro ao adicionar o produto.');
+
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: [error.message],
+      };
+    }
+  }
+
   revalidatePath('/produtos');
   redirect('/produtos');
+
+  // não vai chegar ao return, porém é necessário para o TypeScript
+  return { errors: [] };
 }
--- study-project/src/components/adicionarProduto.tsx
+++ study-project/src/components/adicionarProduto.tsx
@@ -1,22 +1,56 @@
 // components/AdicionarProduto.tsx
+'use client';
+
 import { adicionarProduto } from '@/actions/adicionar-produto';
+import { useFormState, useFormStatus } from 'react-dom';
+
+// Adicionando status pending para evitar múltiplos envios
+function Button() {
+  const status = useFormStatus();
+  return (
+    <div style={{marginTop: '20px'}}>
+      <button type="submit" disabled={status.pending}>
+        Adicionar
+      </button>
+    </div>
+  );
+}
 
 export default function AdicionarProduto() {
+  // Uso do formState é válido no cenário onde, caso o javascript esteja desativado
+  // o envio do formulário ocorrerá normalmente.
+   
+  // Retorna um array, o estado e a ação nova
+  const [state, formAction] = useFormState(adicionarProduto, {
+    errors: [], // Passando o objeto errors para validar erros de formulário
+  });
+
   return (
-    <form action={adicionarProduto}>
+    <form action={formAction}>
       <label htmlFor="nome">Nome</label>
-      <input type="text" id="nome" name="nome" />
+      <input type="text" id="nome" name="nome" required />
+
       <label htmlFor="preco">Preço</label>
-      <input type="number" id="preco" name="preco" />
+      <input type="number" id="preco" name="preco" required />
+
       <label htmlFor="descricao">Descrição</label>
-      <input type="text" id="descricao" name="descricao" />
+      <input type="text" id="descricao" name="descricao" required />
+
       <label htmlFor="estoque">Estoque</label>
-      <input type="number" id="estoque" name="estoque" />
+      <input type="number" id="estoque" name="estoque" required />
+
       <label htmlFor="importado">
         <input type="checkbox" id="importado" name="importado" />
         Importado
       </label>
-      <button type="submit">Adicionar</button>
+
+      {state.errors.map((error, i) => (
+        <p style={{ color: 'red' }} key={i}>
+          {error}
+        </p>
+      ))}
+
+      <Button />
     </form>
   );
 }
