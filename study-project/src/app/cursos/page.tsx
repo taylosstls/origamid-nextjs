37ced4f8a16ec1d4fab2d23916141445cc723f73
taylosstls <luisgustavogto@gmail.com>
2024-05-14 17:05:38 -0300

    Create CSS Module

--- study-project/src/app/cursos/page.tsx
+++ study-project/src/app/cursos/page.tsx
@@ -1,8 +1,9 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import styles from './cursos.module.css';
 
 export default function CursoPage() {
   return (
-    <main>
+    <main className={styles.cursos}>
       <h1>Cursos</h1>
 
       <Link href="/cursos/html/aula/introducao-ao-html">Curso interno</Link>
