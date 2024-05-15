279b26eed94803bde1a718d8fce4325d7fcb97b5
taylosstls <luisgustavogto@gmail.com>
2024-05-15 10:02:58 -0300

    Study Images via source and local images

--- study-project/src/app/animais/page.tsx
+++ study-project/src/app/animais/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import styles from './animais.module.css'
 
 type AnimalsProps = {
   id: number,
@@ -14,11 +15,39 @@ export default async function AnimaisPage() {
   return (
     <main>
       <h1>Animais</h1>
-      {animais.map((animal) => {
-        return <ul key={animal.id}>
-          <li><Image src={animal.imagem} alt={animal.descricao} width={1200} height={800} /></li>
-        </ul>
-      })}
+        
+      <ul className={styles.animais}>
+        {animais.map((animal, i) => {
+        return (
+          /** Qualidade padrão do next 75% e converte a JPG/PNG para webp
+          O sizes gera novas imgs de largura conforme pré-definido no deviceSizes
+          é possível usar mediaquery pra não carregar sempre 100vw, no caso de 2 / 3 / 4 colunas, etc.
+          deviceSizes: [600, 800, 1200, 2400, 3600] */
+          <li key={animal.id}>
+            <p>{animal.nome}</p>
+            <Image
+              src={animal.imagem}
+              alt={animal.descricao}
+              width={1200} height={800} quality={85}
+              sizes="(max-width: 600px) 100vw, 50vw"
+              priority={i < 2} // Prioriza só o carregamento dos dois primeiros, o restante é tudo via lazyload
+              />
+          </li>
+        )
+        }
+      )}
+      </ul>
+
+
+      <h2 style={{ marginTop: '50px' }}>Imagens Locais</h2>
+      <p>Imagem pega da pasta public/imagens</p>
+      <Image
+        src="/imagens/imagem-do-public.jpeg"
+        width={1350}
+        height={1350}
+        alt="Minha foto do Insta"
+        sizes="50vw"
+      />
     </main>
   )
 }
