dc011fec5261b5fc1171b080923b4afacb3aeb9c
taylosstls <luisgustavogto@gmail.com>
2024-05-15 11:12:16 -0300

    Add middleware to check routes

---
+++ study-project/src/middleware.tsx
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  // nova rota
+  if (request.nextUrl.pathname.startsWith('/entrar')) {
+    response.cookies.set('redirecionouUsuario', 'true', {
+      httpOnly: true,
+      secure: true
+    })
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  // verifica se tem o token e se está tentando entrar em /conta
+  const token = request.cookies.get('token')?.value;
+  if (!token && request.nextUrl.pathname.startsWith('/conta')) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+  ],
+};
