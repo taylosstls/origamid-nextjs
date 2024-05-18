f4027f4ce5115738cf227ee5c6f699b0c52d8a50
taylosstls <luisgustavogto@gmail.com>
2024-05-18 19:31:52 -0300

    Create Global Error function and Not Found Page

---
+++ dogs-next/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <section className="container">
+      <h1 className="title">Página não encontrada</h1>
+      <Link className="button" style={{ marginBottom: '1rem', display: 'inline-block' }} href={'/'}>Voltar para a Home.</Link>
+    </section>
+  )
+}
